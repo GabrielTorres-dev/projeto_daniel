7a124abaabf1330e8c162a0d2a3321f6de5063c8
GabrielTorres-dev <gabrielttorres75@gmail.com>
2025-11-05 22:25:57 -0300

    colocando o pedidos

--- Trabalho_um_php/site_principal/conteudo.php
+++ Trabalho_um_php/site_principal/conteudo.php
@@ -1,3 +1,26 @@
 <?php
     echo "<h2> Conteudo</h2>"
 ?>
+<form action="pedidos_inc.php" method="POST">
+    <div class="form-group">
+        <label for="nome">Nome do Cliente:</label>
+        <input type="text" name="nome" id="nome" required>
+    </div>
+    
+    <div class="form-group">
+        <label for="produto">Produto:</label>
+        <input type="text" name="produto" id="produto" required>
+    </div>
+    
+    <div class="form-group">
+        <label for="quantidade">Quantidade:</label>
+        <input type="number" name="quantidade" id="quantidade" min="1" required>
+    </div>
+    
+    <div class="form-group">
+        <label for="endereco">Endereço de Entrega:</label>
+        <textarea name="endereco" id="endereco" required></textarea>
+    </div>
+    
+    <button type="submit">Enviar Pedido</button>
+</form>
